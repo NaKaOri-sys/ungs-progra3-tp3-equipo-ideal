e6afa7be984adfc5994d37e9e0df5226e151fe54
EliseoC45 <eliseo45310@gmail.com>
2026-06-06 22:29:42 -0300

    fix: refactorizacion sobre inyeccion de dependencia y TODOs de MenuController

--- src/equipoideal/controller/MenuController.java
+++ src/equipoideal/controller/MenuController.java
@@ -1,5 +1,8 @@
 package equipoideal.controller;
 
+import equipoideal.model.IncompatibleModel;
+import equipoideal.model.PersonaModel;
+import equipoideal.model.RequerimientoModel;
 import equipoideal.model.listener.IMenuListener;
 import equipoideal.view.MenuView;
 import equipoideal.view.dialogs.IncompatibleDialog;
@@ -12,31 +15,41 @@ import equipoideal.view.dialogs.VentanaEmergente;
 public class MenuController implements IMenuListener {
 	private NavigationController navigationController;
 	private MenuView menuView;
+	
 	private PersonaDialog personaDialog;
 	private RequerimientoDialog requerimientosDialog;
 	private IncompatibleDialog incompatibleDialog;
+	
+	private PersonaController personaController;
+	private RequerimientoController requerimientoController;
 	private IncompatibleController incompatibleController;
+	
+	private PersonaModel personaModel;
+	private RequerimientoModel requerimientoModel;
+	private IncompatibleModel incompatibleModel;
+	
 
 	public MenuController(NavigationController navigationController, MenuView menuView, PersonaDialog personaDialog,
-			RequerimientoDialog requerimientosDialog, IncompatibleDialog incompatibleDialog) {
+			RequerimientoDialog requerimientosDialog, IncompatibleDialog incompatibleDialog,
+			PersonaController personaController, RequerimientoController requerimientoController, IncompatibleController incompatibleController,
+			PersonaModel personaModel, RequerimientoModel requerimientoModel, IncompatibleModel incompatibleModel) {
 		this.navigationController = navigationController;
 		this.menuView = menuView;
 		this.personaDialog = personaDialog;
 		this.requerimientosDialog = requerimientosDialog;
 		this.incompatibleDialog = incompatibleDialog;
+		
+		this.personaModel = personaModel;
+		this.requerimientoModel = requerimientoModel;
+		this.incompatibleModel = incompatibleModel;
+		
+		this.personaController = personaController;
+		this.requerimientoController = requerimientoController;
+		this.incompatibleController = incompatibleController;
 
 		this.menuView.obtenerObserver().addObserver(this);
 	}
 	
-	
-	public void setIncompatibleController(IncompatibleController incompatibleController) {
-        this.incompatibleController = incompatibleController;
-    }
-
-	// TODO agregar los controllers de los dialogs a este controllers ya que es
-	// donde más sentido tiene, los modelos deben ser inyectados desde el main y
-	// pasados a los controllers de los dialogs, para que estos puedan actualizarse
-	// entre sí
 	@Override
 	public void onCargarPersonas() {
 		personaDialog.setVisible(true);
@@ -50,29 +63,27 @@ public class MenuController implements IMenuListener {
 @Override
     public void onIncompatibilidad() {
         
-        if (this.incompatibleController != null) {
-            this.incompatibleController.refrescarPantalla();
-        }
-        
-       //TODO: sacar esto de aca
-        if (incompatibleDialog.getIndexPersona1() == -1) {
+        if (this.personaModel.getListaPersonas().isEmpty()) {
             VentanaEmergente aviso = new VentanaEmergente(null, "Debe cargar personas en el sistema antes de registrar incompatibilidades.");
             aviso.setVisible(true);
-//            return; // Bloquea la apertura
-//        	menuView.mostrarAdvertencia("Debe cargar personas en el sistema antes de registrar incompatibilidades.");
-//        	mainView.mostrarAlertaGlobal("Debe cargar personas en el sistema antes de registrar incompatibilidades.");
             return;
         }
-        
-        
         incompatibleDialog.setVisible(true);
     }
 
-	// TODO si no hay personas, requerimientos o incompatibilidades cargados, debe
-	// estar bloqueada la opción de búsqueda, y mostrar un mensaje indicando que se
-	// deben cargar los datos primero
 	@Override
 	public void onBusqueda() {
+		
+		if(this.personaModel.getListaPersonas().isEmpty())  {
+			VentanaEmergente aviso = new VentanaEmergente(null, "Debe cargar personas en el sistema antes de buscar un equipo ideal.");
+			aviso.setVisible(true);
+			return;
+		}
+		if(this.requerimientoModel.getRequerimientos().isEmpty())  {
+			VentanaEmergente aviso = new VentanaEmergente(null, "Debe cargar requerimientos en el sistema antes de buscar un equipo ideal.");
+			aviso.setVisible(true);
+			return;
+		}
 		this.navigationController.eventSearch();
 	}
 
